2c3405901313c5f3a29db9f973a4786a5f01d54c
Zhrat <Zhrat@DESKTOP-OPL4EDL>
2023-06-01 03:06:21 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+
+        public static List<SpaceShip> SpaceShips = new List<SpaceShip>();
+        public static void Main()
+        {
+            LoadShips();
+            
+
+            bool IsExit = false;
+
+            while (!IsExit)
+            {
+                DisplayMenu();
+                Console.Write("Choose action -> ");
+                var choose = Console.ReadLine();
+                switch (choose)
+                {
+
+                    case "1":
+                        {
+                            CreateShip();
+                            Console.Clear();
+                            break;
+                        }
+                    case "2":
+                        {
+                            ShowShips();
+                            break;
+                        }
+                    case "3":
+                        {
+                            EditShip();
+                            Console.Clear();
+                            break;
+                        }
+                    case "4":
+                        {
+                            SaveShip();
+                            Console.Clear();
+                            break;
+                        }
+                    case "5":
+                        {
+                            Console.WriteLine("Goodbye");
+                            IsExit = true;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Wrong action");
+                            break;
+                        }
+
+                }
+
+            }
+        }
+
+        public static void CreateShip()
+        {
+            
+            SpaceShip Ship = new SpaceShip();
+            Console.WriteLine("Enter ship name");
+            Ship.Name =Console.ReadLine();
+            Console.WriteLine("Enter type ship");
+            Ship.Type =Console.ReadLine();
+            Console.WriteLine("Enter ship speed");
+            Ship.Speed =int.Parse( Console.ReadLine());
+            Console.WriteLine("Enter amout fuel ship");
+            Ship.Vfuel =int.Parse(Console.ReadLine());
+            Ship.Refuel = Ship.Speed/(Ship.Vfuel / 2);
+
+            SpaceShips.Add(Ship);
+
+        }
+        public static void DisplayMenu()
+        {
+
+            Console.WriteLine("1: create ship");
+            Console.WriteLine("2: show all ships");
+            Console.WriteLine("3: edit ships");
+            Console.WriteLine("4: save ships");
+            Console.WriteLine("5: exit");
+
+        }
+
+        public static void ShowShips() => SpaceShips.ForEach(Console.WriteLine);
+
+        public static void EditShip()
+        {
+
+            Console.WriteLine("Choose ship");
+            var index = int.Parse(Console.ReadLine());
+            var Ship = SpaceShips[index-1];
+
+            Console.WriteLine("Enter ship edit name");
+            Ship.Name = Console.ReadLine();
+            Console.WriteLine("Enter edit type ship");
+            Ship.Type = Console.ReadLine();
+            Console.WriteLine("Enter edit ship speed");
+            Ship.Speed = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter edit amout fuel ship");
+            Ship.Vfuel = int.Parse(Console.ReadLine());
+            Ship.Refuel = Ship.Speed / (Ship.Vfuel / 2);
+
+            
+        }
+
+        public static void SaveShip()
+        {
+            var Ships = SpaceShips.Select(s => s.ToString()).ToList();
+            File.WriteAllLines("DataShip.txt",Ships);
+
+        }
+
+        public static void LoadShips()
+            => File.ReadAllLines("DataShip.txt")
+            .Select(SpaceShip.Deserilize).ToList()
+            .ForEach(SpaceShips.Add);
+
+    }
+
+
+}
---
+++ ConsoleApp1/SpaceShip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SpaceShip
+    {
+    public string Name { get; set; }
+     public string Type { get; set; }
+    public int Speed { get; set; }
+    public int Vfuel { get; set; }
+    public int Refuel { get; set; }
+        public override string ToString()
+
+        {
+            return $"{Name} {Type} {Speed} {Vfuel} {Refuel}";
+        }
+
+        public static SpaceShip Deserilize(string value)
+        {
+
+            var args = value.Split(" ");
+
+            return new SpaceShip()
+            {
+
+                Name = args[0],
+                Type = args[1],
+                Speed = int.Parse(args[2]),
+                Vfuel = int.Parse(args[3]),
+                Refuel = int.Parse(args[4])
+
+            };
+
+        }
+    }
+    
+}
